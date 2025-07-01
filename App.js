984b7315a5f65cd6a8d712f512c156f93b1ba592
msoni110 <msoni@beesolvertechnology.in>
2025-07-01 18:09:53 +0530

    Add navigation and initial screens
    Integrated @react-navigation/native and native-stack for navigation. Added SearchScreen and DetailScreen components with basic structure. Updated App.js to use stack navigation and updated dependencies in package.json.

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SearchScreen from "./src/screens/SearchScreen";
+import DetailScreen from "./src/screens/DetailScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Search: SearchScreen,
+    Detail: DetailScreen
   },
 });
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
